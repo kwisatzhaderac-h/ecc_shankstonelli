6068335a6e5ac8220f988758db878c20aba45ca8
steven <steventienlam@gmail.com>
2021-07-18 16:40:16 +1000

    private and public key gen

---
+++ key_gen.py
@@ -0,0 +1,14 @@
+# %%
+import random
+
+def generate_256_hex():
+    # generate random 256-bit hexadecimal number
+    random_number = random.getrandbits(256) # generates random 256 bit number
+    hex_number = hex(random_number) # returns number as hex string
+    return hex_number[2:] # returns string after 0x
+
+### Generate private key
+priv_key = generate_256_hex()
+print(priv_key)
+
+# %%
