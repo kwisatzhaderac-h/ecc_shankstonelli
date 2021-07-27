c7a31fc543076ca9ddb0f8348ad04f97d706d5e3
steven <steventienlam@gmail.com>
2021-07-28 07:47:00 +1000

    main.py can now be called from cli

--- main.py
+++ main.py
@@ -4,12 +4,17 @@
 from shanks import SECP256K1, is_Prime, multiply_Points
 from keygen import generate_256_hex
 
-# %% Executing functions
-# class SECP256k1 has two methods: find_Points() and show_Points()
-pt = SECP256K1(89)
-pt.plot_Points()
-# %%
-pK = int(generate_256_hex(), 16)
-multiply_Points([1, 39], pK, 89)
-# %%
+import sys
+import matplotlib
+
+if __name__ == "__main__":
+    # class SECP256k1 has two methods: find_Points() and show_Points()
+    p = int(input("Please enter a prime number greater than 2: "))
+    pt = SECP256K1(p)
+    pt.plot_Points()
+else:
+    # Executing functions
+    # TODO: show random hex key and multiply it with curve
+    pK = int(generate_256_hex(), 16)
+    multiply_Points()
 # %%
